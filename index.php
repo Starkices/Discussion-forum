6463159d97f6a0e17a014ec61e07d9bcff766a92
Starkices <info.starkices@gmail.com>
2026-08-07 18:36:22 +0100

    Fix login redirection path in index.php for consistency

--- index.php
+++ index.php
@@ -9,6 +9,6 @@ if (!isset($_SESSION['admin_logged_in']) && $_SESSION['user_logged_in'] === true
     exit();
 } else {
     // Not logged in, redirect to login page
-    header('Location: ../login.php');
+    header('Location: ./login.php');
     exit();
 }
